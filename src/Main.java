cbfb62d6010e89647de2aaef76355de553cab392
Maliking <marlon@yahoo.com>
2022-10-07 14:43:10 +0800

    - sop Malik

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import categories.Words;
 public class Main {
     public static void main(String[] args) {
         Game bitin = new Game();
-
+        System.out.println("Hello, Malik!");
     }
 }
