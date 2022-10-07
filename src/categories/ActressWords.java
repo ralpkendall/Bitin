89bf137eff0bf4df4ae9a3dc19a222fbd9998137
ParasoI <ralpkendall@gmail.com>
2022-10-07 21:39:39 +0800

    - Add ActressWords class

---
+++ src/categories/ActressWords.java
@@ -0,0 +1,30 @@
+package categories;
+
+public class ActressWords extends Words{
+
+    public ActressWords() {
+        setWordBank();
+        chooseRandomPhrase();
+        setCategory("Aktres");
+    }
+
+    @Override
+    public void setWordBank() {
+        String[] wordBank = {
+                "Kathryn Bernardo",
+                "Angel Locsin",
+                "Liza Soberano",
+                "Marian Rivera",
+                "Nadine Lustre",
+                "Anne Curtis",
+                "Carla Abellana",
+                "Bianca Umali",
+                "Belle Mariano",
+                "Maja Salvador",
+                "Bea Alonzo",
+                "Kim Chiu",
+        };
+
+        super.setWordBank(wordBank);
+    }
+}
